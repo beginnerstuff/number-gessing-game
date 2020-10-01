333523353fbc5ad1b21d4a6e8431089f6240f9de
Halil Açıkgöz <71661261+Halilackgz@users.noreply.github.com>
2020-10-01 20:27:01 +0300

    Update gessinggame.py

--- gessinggame.py
+++ gessinggame.py
@@ -2,21 +2,27 @@ import random
 
 
 class game:
-    print("Aklından 1 den 10 e kadar bir sayı tut!")
-    number = random.randint(1, 10)
-    tahmin = int(input("tahmininizi giriniz:"))
+    print("Aklınızdan 1 den 10 e kadar bir sayı tutun!")
     sayac = 1
     while True:
-        if tahmin == number:
-            print("tebrikler doğru cevap!")
-            print("tebrikler {}".format(sayac) + ".seferde bildiniz!")
-            input("\nçıkmak için bir tuşa basın.")
-            break
-        elif tahmin >= 10 or tahmin < 1:
-            print("1 ile 10 arasında bir sayı girin lütfen!!")
-            tahmin = int(input("Mesela {}".format(number) + " gibi :"))
-            sayac += 1
-        elif tahmin != number:
-            print("yanlış cevap")
-            tahmin = int(input("tekrar deneyiniz:"))
-            sayac += 1
+        number = random.randint(1, 10)
+        tahmin = int(input("tahmininizi giriniz:"))
+        control = True
+        while control:
+            
+            if tahmin == number:
+                print("tebrikler doğru cevap!")
+                print("tebrikler {}".format(sayac) + ".seferde bildiniz!")
+                input("\nçıkmak için bir tuşa basın.")
+                sayac = 1
+                control = False
+                
+            elif tahmin >= 10 or tahmin < 1:
+                print("1 ile 10 arasında bir sayı girin lütfen!!")
+                tahmin = int(input("Mesela {}".format(number) + " gibi :"))
+                sayac += 1
+            
+            elif tahmin != number:
+                print("yanlış cevap")
+                tahmin = int(input("tekrar deneyiniz:"))
+                sayac += 1
